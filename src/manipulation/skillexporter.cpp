3afc3fe2f553cefe2b167298acb0dd2b6608465c
simon hangl <simon.hangl@uibk.ac.at>
2017-02-09 22:19:06 +0100

    export functionality is ready now - training rnn's tomorrow

--- src/manipulation/skillexporter.cpp
+++ src/manipulation/skillexporter.cpp
@@ -1,6 +1,7 @@
 #include <limits>
 #include <sstream>
 #include <armadillo>
+#include <kukadu/utils.hpp>
 #include <kukadu/manipulation/skillexporter.hpp>
 #include <kukadu/storage/sensorstoragesingleton.hpp>
 
@@ -8,6 +9,12 @@ using namespace std;
 using namespace arma;
 
 namespace kukadu {
+
+    void storeExecution(vector<KUKADU_SHARED_PTR<Hardware> >& hardwareInstances,
+                        map<int, vector<vector<std::pair<long long int, arma::vec> > > >& allSamples, int& executionId,
+                        long long int& maxDuration, long long int& minDeltaT,
+                        vector<int>& sensorDataLengthPerHardware,
+                        std::string file);
 
     SkillExporter::SkillExporter(StorageSingleton &storage) : StorageHolder(storage) {
 
@@ -95,7 +102,21 @@ namespace kukadu {
 
     }
 
+    std::pair<int, int> msToMinSec(long long int time) {
+
+        double totalSeconds = (double) time / 1000.0;
+        int totalMins = totalSeconds / 60.0;
+        int restSeconds = (totalSeconds - totalMins * 60.0);
+
+        return {totalMins, restSeconds};
+
+    }
+
     void SkillExporter::exportSkillExecutions(int skillId, long long int startTime, long long int endTime, std::string folder) {
+
+        if(fileExists(folder))
+            throw KukaduException("(SkillExporter) target directory already exists");
+        createDirectory(folder);
 
         auto& sensorSingleton = SensorStorageSingleton::get();
 
@@ -110,7 +131,9 @@ namespace kukadu {
 
         long long int maxDuration = 0;
         long long int minDeltaT = std::numeric_limits<long long int>::max();
-        vector<vector<std::pair<long long int, arma::vec> > > hardwareData;
+        map<int, vector<vector<std::pair<long long int, arma::vec> > > > allSamples;
+        for(auto& hw : hardwareInstances)
+            allSamples[hw->getHardwareInstance()] = vector<vector<std::pair<long long int, arma::vec> > >{};
 
         int exportedExecutionCount = 0;
         auto executions = getSkillExecutions(skillId, startTime, endTime);
@@ -120,9 +143,9 @@ namespace kukadu {
             long long int endTime = get<1>(execution);
             bool succ = get<2>(execution);
 
-            double totalSeconds = (double) (endTime - startTime) / 1000.0;
-            int totalMins = totalSeconds / 60.0;
-            int restSeconds = (totalSeconds - totalMins * 60.0);
+            auto convTime = msToMinSec(endTime - startTime);
+            int totalMins = convTime.first;
+            int restSeconds = convTime.second;
 
             cout << "exporting execution number " << ++exportedExecutionCount;
             if(endTime > 0)
@@ -134,8 +157,8 @@ namespace kukadu {
             for(auto& hw : hardwareInstances) {
 
                 // load all the data
-                hardwareData.push_back(hw->loadData(startTime, endTime));
-                auto& executionData = hardwareData.back();
+                allSamples[hw->getHardwareInstance()].push_back(hw->loadData(startTime, endTime));
+                auto& executionData = allSamples[hw->getHardwareInstance()].back();
 
                 if(executionData.size()) {
 
@@ -171,28 +194,139 @@ namespace kukadu {
 
             }
 
-            int totalDimensionality = 0;
-            for(auto& senorDataLength : sensorDataLengthPerHardware)
-                totalDimensionality += senorDataLength;
+        }
 
-            cout << "statistcis on data:" << endl;
-            cout << "\t Maximal duration: " << maxDuration << "ms" << endl;
-            cout << "\t Minimal delta t: " << minDeltaT << "ms" << endl;
-            cout << "\t Total sensor dimensionality: " << totalDimensionality << endl;
+        // compute the sum of all dimension
+        int totalDimensionality = 0;
+        for(auto& senorDataLength : sensorDataLengthPerHardware)
+            totalDimensionality += senorDataLength;
 
-            // create the storage grid
-            // the grid resolution is the maximum frequency; the length is the maximal duration of all executions
-            int maxTimeIdx = ceil((double) maxDuration / minDeltaT);
-            mat storageGrid(totalDimensionality, maxTimeIdx);
+        auto convTime = msToMinSec(maxDuration);
+        int maxMins = convTime.first;
+        int maxRestSeconds = convTime.second;
 
-            /*
-            // fill the storage grid
-            for(auto& hwData : hardwareData) {
+        cout << "statistics on data:" << endl;
+        cout << "\t Maximal duration: " << maxMins << "m:" << maxRestSeconds << "s" << endl;
+        cout << "\t Minimal delta t: " << minDeltaT << "ms" << endl;
+        cout << "\t Total sensor dimensionality: " << totalDimensionality << endl;
 
-            }
-            */
+        if(*(folder.end()) != '/')
+            folder += "/";
+
+        ofstream labelFile;
+        labelFile.open(string(folder + "labels").c_str());
+
+        ofstream labelDetailFile;
+        labelDetailFile.open(string(folder + "labels_detail").c_str());
+
+        for(int i = 0; i < executions.size(); ++i) {
+
+            auto& execution = executions.at(i);
+            bool succ = get<2>(execution);
+            long long int endTime = get<1>(execution);
+
+            int successMode = -1;
+
+            if(succ)
+                successMode = 0;
+            else if(endTime != 0)
+                successMode = 1;
+            else
+                successMode = 2;
+
+            stringstream s;
+            s << "skill_" << i;
+            labelFile << s.str() << "\t" << succ << endl;
+            labelDetailFile << s.str() << "\t" << successMode << endl;
+
+            storeExecution(hardwareInstances, allSamples, i, maxDuration, minDeltaT, sensorDataLengthPerHardware, folder + s.str());
 
         }
+
+
+    }
+
+    void storeExecution(vector<KUKADU_SHARED_PTR<Hardware> >& hardwareInstances,
+                        map<int, vector<vector<std::pair<long long int, arma::vec> > > >& allSamples, int& executionId,
+                        long long int& maxDuration, long long int& minDeltaT,
+                        vector<int>& sensorDataLengthPerHardware,
+                        std::string file) {
+
+        int totalDimensionality = 0;
+        for(auto& senorDataLength : sensorDataLengthPerHardware)
+            totalDimensionality += senorDataLength;
+
+        // create the storage grid
+        // the grid resolution is the maximum frequency; the length is the maximal duration of all executions
+        int maxTimeIdx = ceil((double) maxDuration / minDeltaT);
+        mat storageGrid(totalDimensionality, maxTimeIdx);
+        storageGrid.fill(datum::nan);
+
+        ofstream outFile;
+        outFile.open(file.c_str());
+
+        vector<int> maxFilledColId(totalDimensionality, 0);
+
+        // filling the grid
+        // run through all the data for each hardware and fill it to the corresponding spot in the matrix
+        int dimOffset = 0;
+        for(int i = 0; i < hardwareInstances.size(); ++i) {
+
+            auto& dataPerHardware = allSamples[hardwareInstances.at(i)->getHardwareInstance()];
+
+            // go through all executions
+            auto& executionPerHardware = dataPerHardware.at(executionId);
+
+            // go through all sensor samples for each execution
+            for(auto& dataLine : executionPerHardware) {
+
+                auto& currentTime = dataLine.first;
+                auto& currentSensorSample = dataLine.second;
+
+                // store the sample to the current position in the storage grid
+                int currentColIdx = (double) currentTime / minDeltaT;
+                for(int j = 0; j < sensorDataLengthPerHardware.at(i); ++j) {
+
+                    maxFilledColId.at(dimOffset + j) = currentColIdx;
+                    storageGrid(dimOffset + j, currentColIdx) = currentSensorSample(j);
+
+                    // from the current time index go back until the last sample (all values until there are NaN) and interpolate
+                    int k = 0;
+                    for(k = currentColIdx - 1; k >= 0; --k) {
+                        // check if value is nan --> if not, stop the loop, we reached the interpolation place
+                        if(storageGrid(dimOffset + j, k) == storageGrid(dimOffset + j, k))
+                            break;
+                    }
+
+                    // if k is smaller than 0, it means that all values before were nan --> no interpolation possible, just fill it up with the current value
+                    if(k < 0)
+                        for(int l = 0; l < currentColIdx; ++l)
+                            storageGrid(dimOffset + j, currentColIdx) = currentSensorSample(j);
+                    else if(k != currentColIdx) {
+                        double deltaX = (double) (storageGrid(dimOffset + j, currentColIdx) - storageGrid(dimOffset + j, k)) / (currentColIdx - k);
+                        for(int l = k; l < currentColIdx; ++l)
+                            storageGrid(dimOffset + j, l) = storageGrid(dimOffset + j, k) + l * deltaX;
+                    }
+
+                }
+
+            }
+
+            dimOffset += sensorDataLengthPerHardware.at(i);
+
+        }
+
+        // at this stage the begin of the grid should be free of nans --> now fill up the back part with dummy data
+        for(int i = 0; i < maxFilledColId.size(); ++i) {
+            auto& currentMaxColId = maxFilledColId.at(i);
+            if(currentMaxColId == 0)
+                cout << "(SkillExporter) warning: not all hardware components delivered data (row: " << i << " was empty)" << endl;
+            for(int j = currentMaxColId + 1; j < storageGrid.n_cols; ++j)
+                storageGrid(i, j) = storageGrid(i, currentMaxColId);
+        }
+
+        outFile << storageGrid << endl;
+        outFile.close();
 
     }
 
