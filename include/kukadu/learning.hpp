72e00706d8beb030d6386f601bde54d759c217d2
Simon Walter Hangl <simon.hangl@uibk.ac.at>
2017-04-07 20:16:16 +0200

    added lemon library

--- include/kukadu/learning.hpp
+++ include/kukadu/learning.hpp
@@ -3,9 +3,8 @@
 #include <kukadu/learning/clustering/kmeans.hpp>
 #include <kukadu/learning/regression/kernel.hpp>
 #include <kukadu/learning/classification/classifier.hpp>
-#include <kukadu/learning/metric_learning/metriclearner.hpp>
-#include <kukadu/learning/classification/libsvmclassifier.hpp>
-#ifndef USEBOOST
 #include <kukadu/learning/projective_simulation/core.hpp>
 #include <kukadu/learning/projective_simulation/clips.hpp>
-#endif
+#include <kukadu/learning/metric_learning/metriclearner.hpp>
+#include <kukadu/learning/classification/libsvmclassifier.hpp>
+#include <kukadu/learning/projective_simulation/visualization.hpp>
