b1d3ee9d7fdda9389bd8f9c5cb242316e5497402
agyss <agyss624@hotmail.com>
2017-07-06 17:16:00 +0200

    added functionality to generate catkin project from blockly

--- include/kukadu/gui/graphical.hpp
+++ include/kukadu/gui/graphical.hpp
@@ -1,11 +1,17 @@
 #ifndef KUKADU_GRAPHICAL_H
 #define KUKADU_GRAPHICAL_H
 
+#include <QtWidgets/QLayout>
+#include <QtCore/QString>
 #include <QtWidgets/QWidget>
 #include <QAbstractTableModel>
 #include <QtWidgets/QGroupBox>
 #include <QItemSelectionModel>
 #include <QtWidgets/QTabWidget>
+#include <QtWebKitWidgets/QWebView>
+#include <QtWebKitWidgets/QWebPage>
+#include <QtWebKitWidgets/QWebFrame>
+#include <stdio.h>
 
 #include <string>
 #include <vector>
@@ -15,22 +21,26 @@
 namespace kukadu {
 
     class KukaduGraphical : public QWidget {
-
         Q_OBJECT
 
     private:
 
-        static auto constexpr DEFAULT_WIDTH = 800;
-        static auto constexpr DEFAULT_HEIGHT = 600;
+        static auto constexpr DEFAULT_WIDTH = 1600;
+        static auto constexpr DEFAULT_HEIGHT = 800;
 
         QTabWidget* mainTab;
+        QWebView* webView;
 
-        QGroupBox* generateWebview();
+        QGroupBox* createUI();
 
     public:
 
         explicit KukaduGraphical();
         ~KukaduGraphical();
+
+    public slots:
+
+      void clickedSlot();
 
     };
 
--- src/gui/graphical.cpp
+++ src/gui/graphical.cpp
@@ -2,12 +2,14 @@
 
 #include <sstream>
 #include <iostream>
+#include <vector>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QListView>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QTableView>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QMessageBox>
 #include <QtWebKit/QtWebKit>
 
@@ -15,45 +17,104 @@
 //#include <QtWidgets/../QtWebKitWidgets/QWebView>
 //#include <QtWidgets/../QtWebKitWidgets/QWebPage>
 //#include <QtWidgets/../QtWebKitWidgets/QWebFrame>
-#include <QtWebKitWidgets/QWebView>
-#include <QtWebKitWidgets/QWebPage>
-#include <QtWebKitWidgets/QWebFrame>
+
 
 using namespace std;
 
 namespace kukadu {
 
     KukaduGraphical::KukaduGraphical() {
-
         mainTab = new QTabWidget(this);
-
-        auto robotBox = generateWebview();
-        mainTab->addTab(robotBox, "Robots");
-
         mainTab->setMinimumSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        mainTab->setMaximumSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
+        auto tab = createUI();
+        mainTab->addTab(tab, "Blockly");
     }
 
     KukaduGraphical::~KukaduGraphical() {
 
     }
 
-    QGroupBox* KukaduGraphical::generateWebview() {
-        auto mainGroupBox= new QGroupBox();
-        auto layoutOfMainGroupBox= new QGridLayout();
+    QGroupBox* KukaduGraphical::createUI() {
+        auto mainView = new QGroupBox();
+        auto mainLayout = new QGridLayout();
+        auto buttonContainer = new QHBoxLayout();
 
-        auto robotListBox = new QWebView(this);
+        QWebSettings::globalSettings()->setAttribute(QWebSettings::DeveloperExtrasEnabled, true);
+        QWebSettings::globalSettings()->setAttribute(QWebSettings::LocalContentCanAccessRemoteUrls,true);
+        QWebSettings::globalSettings()->setAttribute(QWebSettings::LocalContentCanAccessFileUrls,true);
+
+        auto frame = new QGroupBox();
+        frame->setLayout(mainLayout);
+
+        webView = new QWebView(this);
+        webView->setMinimumSize(DEFAULT_WIDTH, DEFAULT_HEIGHT-150);
+        webView->setMaximumSize(DEFAULT_WIDTH, DEFAULT_HEIGHT-150);
+        webView->load(QUrl("file:///home/agyss/blockly\ test/cake/test.html"));
+
+        auto executeButton = new QPushButton("Execute");
+        QObject::connect(executeButton, SIGNAL(clicked()),this, SLOT(clickedSlot()));
 
 
-        robotListBox->setMinimumSize(DEFAULT_WIDTH / 3.0, DEFAULT_HEIGHT / 3.0);
-        robotListBox->setMaximumSize(DEFAULT_WIDTH / 3.0, DEFAULT_HEIGHT / 3.0);
+        mainView->setLayout(mainLayout);
+        mainLayout->addLayout(buttonContainer, 1, 0);
+        mainLayout->addWidget(webView, 0, 0);
+        buttonContainer->addWidget(executeButton);
 
-        layoutOfMainGroupBox->addWidget(robotListBox, 0, 0);
+        return mainView;
+    }
 
-        mainGroupBox->setLayout(layoutOfMainGroupBox);
-        mainGroupBox->setMinimumSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-        mainGroupBox->setMaximumSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    void KukaduGraphical::clickedSlot() {
+        QVariant codeVariant = webView->page()->mainFrame()->evaluateJavaScript("getCode()");
+        qDebug() << codeVariant.toString();
+        system("cd ~/iis_robot_sw/iis_catkin_ws/src/;rm -r test;catkin_create_pkg test geometry_msgs kukadu;cd test;mkdir src;");
 
-        return mainGroupBox;
+
+        QFile codeFile("/home/agyss/iis_robot_sw/iis_catkin_ws/src/test/src/code.cpp");
+        if(codeFile.exists()){
+            codeFile.remove();
+        }
+
+        codeFile.open(QIODevice::WriteOnly);
+        QString code = codeVariant.toString();
+        QByteArray codeByteArray = code.toLatin1();
+        codeFile.write(codeByteArray.data(), code.length());
+        codeFile.close();
+
+
+        QFile cmakeInFile("/home/agyss/iis_robot_sw/iis_catkin_ws/src/test/CMakeLists.txt");
+        QFile cmakeOutFile("/home/agyss/iis_robot_sw/iis_catkin_ws/src/test/CMakeLists.txt1");
+        cmakeInFile.open(QIODevice::ReadOnly);
+        cmakeOutFile.open(QIODevice::WriteOnly);
+
+        QTextStream streamIn(&cmakeInFile), streamOut(&cmakeOutFile);
+
+        int arraysize = 124;
+        QString textToAdd[arraysize];
+        textToAdd[3] = QString("set(CMAKE_BUILD_TYPE Debug)");
+        textToAdd[5] = QString("include(CheckCXXCompilerFlag)\r\nCHECK_CXX_COMPILER_FLAG(\"-std=c++11\" COMPILER_SUPPORTS_CXX11)\r\nCHECK_CXX_COMPILER_FLAG(\"-std=c++0x\" COMPILER_SUPPORTS_CXX0X)\r\nif(COMPILER_SUPPORTS_CXX11)\r\nset(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -std=c++11\")\r\nadd_definitions(-DCPP11SUPPORTED)\r\nelseif(COMPILER_SUPPORTS_CXX0X)\r\nset(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -std=c++0x\")\r\nadd_definitions(-DCPP11SUPPORTED)\r\nelse()\r\nmessage(STATUS \"The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.\")\r\nendif()");
+        textToAdd[123] = QString("add_executable(test src/code.cpp)\r\ntarget_link_libraries(test ${catkin_LIBRARIES} kukadu kukaduvision)");
+        int i = 0;
+        while (!streamIn.atEnd()){
+            if(i < arraysize){
+                auto data = textToAdd[i];
+                if(data != NULL){
+                    streamOut << data << "\r\n";
+                }
+            }
+
+            QString line = streamIn.readLine();
+            if(!line.startsWith("#")){
+                streamOut << line << "\r\n";
+            }
+
+            i++;
+        }
+        cmakeInFile.close();
+        cmakeOutFile.close();
+
+        cmakeInFile.remove();
+        cmakeOutFile.rename("/home/agyss/iis_robot_sw/iis_catkin_ws/src/test/CMakeLists.txt");
     }
 }
